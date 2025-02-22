e1bf4d1b690511c7d6f7abd804366c2df5347158
dinchen1089 <>
2025-02-23 04:11:20 +0530

    deleted the older folder

--- fit_body/src/utils/dimension.js
+++
@@ -1,6 +0,0 @@
-import { Dimensions } from "react-native";
-
-const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("screen");
-const { width: WINDOW_WIDTH, height: WINDOW_HEIGHT } = Dimensions.get("window");
-
-export { SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT };
--- fit_body/src/utils/platform/detection.js
+++
@@ -1,6 +0,0 @@
-import { Platform } from "react-native";
-
-export const isIOS = Platform.OS === "ios";
-export const isAndroid = Platform.OS === "android";
-export const isNative = isIOS || isAndroid;
-export const isWeb = !isNative;
